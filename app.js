45fcd425d525839149ebd5605ea3fda04a3e0c3f
RampantOwl <trygve@grimnes.org>
2014-10-10 20:05:20 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var fs       = require( 'fs' )
+var util     = require( 'util' )
+var path     = require( 'path' )
+
+var async    = require( 'async' )
+var winston  = require('winston')
+
+var Elasticsearch = require( 'winston-elasticsearch' )
+
+require('winston-fluent').Fluent;
+
+var config   = require( './config/config' )
+
+var DATA_SUBSTR_OFFSET = 5
+var DATA_DIR_NAME_FILTER = "_data"
+global.logger = new (winston.Logger)({
+    transports: [
+	new (winston.transports.Console)( { 'timestamp' : true } ),
+	new Elasticsearch( { level : 'info' } ),
+    ]
+})
+
+global.logger.info( "Started" , { dir : config.datadir })
+
+fs.readdir( path.resolve( __dirname , config.datadir ), function( err , dirname ) {
+    var count = Object.keys( dirname ).length
+
+    for ( var n = 0; count > n; n++ )
+	if( dirname[n].substring( dirname[n].length - DATA_SUBSTR_OFFSET )  == DATA_DIR_NAME_FILTER )
+	    global.logger.info( "Match Directory" ,  { dir : dirname[n] } )
+})
+
---
+++ reader.js
@@ -0,0 +1,11 @@
+var config = {
+    name : 'Ebola Reader'
+    fluent : {
+	tag  : 'ebola',
+	label : 'data',
+	host : 'hyperion.zoopit.no',
+	port : 24224,
+	timeout : 3,
+	timestamp : true
+    }
+}

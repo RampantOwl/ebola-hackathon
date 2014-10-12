c008b5af883f6866c2e6c49a7ccf2d382784b57c
RampantOwl <trygve@grimnes.org>
2014-10-12 13:52:46 +0200

    Added NaN check

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function csvToElastic( file , callback){
 			doc : doc
 		    }
 		    
-		    if( typeof( data ) != "undefined" && data !="" && data !=null)
+		    if( typeof( data ) != "undefined" && data !="" && data !=null && isNan(data)==false)
 			funcs.push( function( callback ){
 			    global.client.core.index( object ,callback )
 			})

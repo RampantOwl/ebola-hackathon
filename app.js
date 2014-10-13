b95aebd9a203c2a387a18850111ebddff687a414
RampantOwl <trygve@grimnes.org>
2014-10-13 08:37:03 +0200

    Country name from basedir again

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ function csvToElastic( file , callback){
     var file_data = fs.readFileSync( file ).toString()
     var file_path_seperated = file.split(path.sep)
     
-    var country = path.basename(file).substr(0,path.basename(file).indexOf("_case_data"))
+    var file_dir = file_path_seperated[file_path_seperated.length -2]
+    var country = file_dir.substr(0,file_dir.indexOf("_data"))
     console.log( "Country Name: %s" ,country )
     /* Takes csv data and returns 2-dimentional array of its contents */
     parse(file_data , function( err , lines ) {	
